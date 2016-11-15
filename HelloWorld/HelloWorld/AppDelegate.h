1c5dd8dfafbc0800fba3b776bdf1021404d0ad76
周彬涵 <zhoubinhan@chinamobile.com>
2016-11-15 11:18:30 +0800

    no message

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by 周彬涵 on 2016/11/15.
+//  Copyright © 2016年 周彬涵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by 周彬涵 on 2016/11/15.
+//  Copyright © 2016年 周彬涵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
